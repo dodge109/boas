dd00996e86bf9bc50cf5800d4987c60e6d5f7893
dodge109 <fadyjunior252@gmail.com>
2026-05-03 21:01:12 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{padding: '50px', fontFamily: 'Arial', textAlign: 'center'}}>
+      <h1>DEU CERTO CARALHO! 🚀</h1>
+      <p>Site Vite + React no ar na Vercel.</p>
+      <p>Erro handleInvalidResolvedId: MORREU!</p>
+    </div>
+  )
+}
+
+export default App
